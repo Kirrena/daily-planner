cf7e88162bd5289235f4f77aa00fe5ad0a6d0802
Renata Kiraly <kirrena@gmail.com>
2023-11-10 23:40:55 +0000

    added today date to header

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+//select p tag with currentDay ID
+var currentDay = $("#currentDay");
+//store today date in a variable with dayjs
+var actualDate = dayjs();
+//display today date
+currentDay.text(actualDate.format("dddd, MMMM D[th]"));
